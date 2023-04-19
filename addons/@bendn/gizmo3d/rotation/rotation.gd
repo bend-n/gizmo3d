a554c81f6ca27af4155d22291c2e6215a6113878
bendn <bend.n@outlook.com>
2023-04-19 20:03:34 +0700

    scale down rot gizmo & make materials not say condition "!material" is true

--- addons/@bendn/gizmo3d/rotation/rotation.gd
+++ addons/@bendn/gizmo3d/rotation/rotation.gd
@@ -6,8 +6,8 @@ class_name RotationGizmo
 
 func _input(event: InputEvent) -> void:
 	if rotate_cw.matches_event(event) and event.is_pressed():
-		rotated.emit(Vector3(0, PI/2, 0))
+		rotated.emit(Vector3(0, -(PI/2), 0))
 		finalize.emit()
 	elif rotate_ccw.matches_event(event) and event.is_pressed():
-		rotated.emit(Vector3(0, -(PI/2), 0))
+		rotated.emit(Vector3(0, PI/2, 0))
 		finalize.emit()
